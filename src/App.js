db13e2f8bf4ff143e97f889f62ff8439a9e768b8
Entropiks <mikecorseri@gmail.com>
2020-08-21 21:18:27 -0700

    new api pulls + styling

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchOriginals} />
-      <Row title="Trending Now" />
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchOriginals} 
+        isLargeRow
+        />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Popular Movies" fetchUrl={requests.fetchPopular} />
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,9 @@
 const API_KEY = "7ce64cf5ef7bf9d74fef966c4e86de10";
 
 const api_requests = {
-    fetchOriginals: `/discover/tv?api_key=${API_KEY}`
+    fetchOriginals: `/discover/tv?api_key=${API_KEY}`,
+    fetchTrending: `/trending/all/day?api_key=${API_KEY}`,
+    fetchPopular: `/movie/popular?api_key=${API_KEY}`
 }
 
 export default api_requests;
